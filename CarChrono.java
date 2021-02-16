c35002d408e87a1ce1bb675996539909d208ec43
deShortOne <55034724+brianheather@users.noreply.github.com>
2021-02-16 10:04:01 +0000

    Update CarChrono.java

--- CarChrono.java
+++ CarChrono.java
@@ -3,101 +3,125 @@
  * Combined checkRoute and addRoute -->method: insertRoute
  */
 package application;
+
 import java.util.ArrayList;
 //import java.util.Arrays;
 
 public class CarChrono {
-	
-	//private ArrayList<Integer[]> range = new ArrayList<>();
+
+	// private ArrayList<Integer[]> range = new ArrayList<>();
 	private ArrayList<Route> deRoute = new ArrayList<>();
-	
+
 	public CarChrono() {
-		//range.add(new Integer[] {0});
-		deRoute.add(new Route(0,0,0,0,0,0,0));
+		// range.add(new Integer[] {0});
+		deRoute.add(new Route(0, 0, 0, 0, 0, 0, 0));
 	}
-	
-	public int insertRoute(Route routeB) {
+
+	public boolean insertRoute(Route routeB) {
+		System.out.println("AA");
 		int startT = routeB.getStartT();
 		int endT = routeB.getEndT();
-		
+
 		int loc = findGoodTRoute(startT, endT);
-		
-		Route routeA = deRoute.get(loc);
-		Route routeC = deRoute.get(loc+1); //Error if at end
-		
-		if (goodTravelTime(routeA, routeB) && goodTravelTime(routeB, routeC)) {
-			deRoute.add(loc+1, routeB);
-			return loc;
+
+		System.out.println(loc);
+		if (loc != -1) {
+			System.out.println("SHOULD BE ADDING");
+			Route routeA = deRoute.get(loc);
+	
+			System.out.println("AB");
+			if (goodTravelTime(routeA, routeB)) {
+				if (loc + 1 == deRoute.size()) {
+					deRoute.add(routeB);
+					System.out.println("AC, ADDED");
+					return true;
+				} else {
+					Route routeC = deRoute.get(loc + 1);
+					if (goodTravelTime(routeB, routeC)) {
+						deRoute.add(loc + 1, routeB);
+						return true;
+					}
+					return false;
+				} 
+			} else {
+				return false;
+			}
 		} else {
-			return -1;
+			System.out.println("ASDFASDF-1");
+			return false;
 		}
 	}
-	
-	/* true can get in time
-	 * false can't get in time 
+
+	/*
+	 * true can get in time false can't get in time
 	 */
 	public boolean goodTravelTime(Route routeA, Route routeB) {
 		int endT = routeA.getEndT();
 		int startT = routeB.getStartT();
-		int timeNeeded = Math.abs(endT-startT);
-		
+		int timeNeeded = Math.abs(endT - startT);
+
 		int[] loc1 = routeA.getEndL();
 		int[] loc2 = routeB.getStartL();
 		int dist = calDist(loc1[0], loc1[1], loc2[0], loc2[1]);
-		
-		return  timeNeeded >= dist;
-	}
-	
-	public int findGoodTRoute(int startT, int endT) {
-		int num = -1;
-		for(int i = 0; i < deRoute.size(); i++) {
-			Route elem = deRoute.get(i);
-			if (elem.getEndT() >= startT) {
-				if (deRoute.get(i+1).getStartT() <= endT) {
-					num = i;
-					break;
-				}
-			}
-		}
-		return num;
-	}
-	
-	//Found in MainChrono
-	public static int calDist(int x1, int y1, int x2, int y2) {
-		return Math.abs(x1-x2) + Math.abs(y2 - y1);
+
+		return timeNeeded >= dist;
 	}
 
-	/*Not needed
-	public void addRouteVoid(Route route) {
-		int num = findGoodTRoute(route.getStartT(), route.getEndT());
-		deRoute.add(num+1, route);
+	//Goes through deRoute and finds a place where the route could be inserted
+	//Doesn't take distance into account
+	public int findGoodTRoute(int startT, int endT) {
+		int num = -1;
 		
-		/*
-		for (int i = 0; i < range.size(); i++) {
-			if (range.get(i)[1] > route.getStartT()) {
-				range.add(i+1, new Integer[] {route.getStartT(),route.getEndT()});
-				break;
+		System.out.println("BA");
+		for (int i = 0; i < deRoute.size(); i++) {
+			System.out.println("BB");
+			Route elem = deRoute.get(i);
+			if (elem.getEndT() <= startT) {
+				if (i == deRoute.size() - 1) {
+					System.out.println("BC");
+					num = i;
+				} else {
+					if (endT <= deRoute.get(i + 1).getStartT()) {
+						System.out.println("BD");
+						num = i;
+						break;
+					}
+				}
+
 			}
 		}
-		// * /
+		System.out.println("BE");
+		return num;
 	}
-	
-	public void addRouteEnd(Route route) {
-		Route lastPos = deRoute.get(deRoute.size() - 1);
-		int[] endxy = lastPos.getEndL();
-		
-		int[] startxy = route.getStartL();
-		
-		if (Arrays.equals(endxy, startxy)) {
-			
-		}
-		deRoute.add(route);
+
+	// Found in MainChrono
+	public static int calDist(int x1, int y1, int x2, int y2) {
+		return Math.abs(x1 - x2) + Math.abs(y2 - y1);
 	}
-	
-	
-	//Not done
-	public boolean possibleBefore(Route prevPos) {
-		return true;
+
+	public ArrayList<Route> getRoutes() {
+		return deRoute;
 	}
-	*/
+
+	/*
+	 * Not needed public void addRouteVoid(Route route) { int num =
+	 * findGoodTRoute(route.getStartT(), route.getEndT()); deRoute.add(num+1,
+	 * route);
+	 * 
+	 * /* for (int i = 0; i < range.size(); i++) { if (range.get(i)[1] >
+	 * route.getStartT()) { range.add(i+1, new Integer[]
+	 * {route.getStartT(),route.getEndT()}); break; } } // * / }
+	 * 
+	 * public void addRouteEnd(Route route) { Route lastPos =
+	 * deRoute.get(deRoute.size() - 1); int[] endxy = lastPos.getEndL();
+	 * 
+	 * int[] startxy = route.getStartL();
+	 * 
+	 * if (Arrays.equals(endxy, startxy)) {
+	 * 
+	 * } deRoute.add(route); }
+	 * 
+	 * 
+	 * //Not done public boolean possibleBefore(Route prevPos) { return true; }
+	 */
 }
